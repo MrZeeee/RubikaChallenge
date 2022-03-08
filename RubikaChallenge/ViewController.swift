9e25957efd37f25bdf7176bfdd187b6e424e3c6f
MohammadReza <mrezamanieh@gmail.com>
2022-03-08 20:34:32 +0330

    Initial Commit

---
+++ RubikaChallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RubikaChallenge
+//
+//  Created by MohammadReza Zamanieh on 3/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
